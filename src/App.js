1d0d2ba5e1247115a04b655b703eb6a374c5ce2d
Jean <1jeanbispo@gmail.com>
2020-03-01 19:16:06 -0300

    link stylesheet in index.html

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 import Routes from "./routes";
 import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <>
-      <Helmet>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/jeanbispo/reey-font@1.10/stylesheet.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
-        />
-      </Helmet>
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
