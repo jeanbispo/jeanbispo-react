028842c901ca09b15691e47477556d25cc13fbcd
Jean <1jeanbispo@gmail.com>
2020-03-01 19:16:22 -0300

    routes transitions effects

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,54 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import styled from "styled-components";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 function Routes() {
+  const location = useLocation();
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/about" component={About} />
-    </Switch>
+    <Container>
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          timeout={{ enter: 300, exit: 300 }}
+          classNames="fade"
+        >
+          <Switch location={location}>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </Container>
   );
 }
+
+const Container = styled.main`
+  height: 100%;
+
+  & > * {
+    height: 100%;
+  }
+
+  .fade-enter {
+    opacity: 0.01;
+  }
+
+  .fade-enter.fade-enter-active {
+    opacity: 1;
+    transition: opacity 300ms ease-in;
+  }
+
+  .fade-exit {
+    opacity: 1;
+  }
+
+  .fade-exit.fade-exit-active {
+    opacity: 0.01;
+    transition: opacity 300ms ease-in;
+  }
+`;
 
 export default Routes;
